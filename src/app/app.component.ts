a9f8cc2d92691a0ff691ea1daed62c17f31384af
Azamat Salamat <azamat.salamat@samgau.com>
2024-08-24 16:11:49 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { MenuInputComponent } from "./menu-input/menu-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MenuInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/menu-input/menu-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-input.component.html',
+  styleUrl: './menu-input.component.css'
+})
+export class MenuInputComponent {
+
+}
