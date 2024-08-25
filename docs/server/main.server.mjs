7245dd52372265b33cc3959398ad933ea7f153c1
Azamat Salamat <azamat.salamat@samgau.com>
2024-08-25 17:45:00 +0500

    update build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2OVAMUGK.mjs";import"./chunk-QBENAA6F.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KC4J25NY.mjs";import"./chunk-EFUJTTTS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
